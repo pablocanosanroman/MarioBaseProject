d347aac5de533d1bb9d1cf66c98780fda64c0920
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-02-08 19:44:56 +0000

    Tut7 Progression (Error)

---
+++ MarioBaseProject/MarioBaseProject/Character.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef _CHARACTER_H
+#define _CHARACTER_H
+
+#include <SDL.h>
+#include <iostream>
+#include "Commons.h"
+
+
+class Texture2D;
+
+class Character
+{
+protected:
+
+	SDL_Renderer* m_renderer;
+	Vector2D m_position;
+	Texture2D* m_texture;
+
+public:
+
+	Character(SDL_Renderer* renderer, std::string imagePath, Vector2D start_position);
+	~Character();
+
+	virtual void Render();
+	virtual void Update(float deltaTime, SDL_Event e);
+	void SetPosition(Vector2D new_position);
+	Vector2D GetPosition();
+};
+
+#endif _CHARACTER_H //_CHARACTER_H
--- MarioBaseProject/MarioBaseProject/GameScreenLevel1.cpp
+++ MarioBaseProject/MarioBaseProject/GameScreenLevel1.cpp
@@ -2,6 +2,9 @@
 #include <iostream>
 #include "Texture2D.h"
 
+
+
+
 GameScreenLevel1::GameScreenLevel1(SDL_Renderer* renderer) : GameScreen(renderer)
 {
 	SetUpLevel1();
@@ -12,25 +15,32 @@ GameScreenLevel1::~GameScreenLevel1()
 	delete m_background_texture;
 
 	m_background_texture = nullptr;
+
+	delete my_character;
+
+	my_character = nullptr;
 }
 
 void GameScreenLevel1::Render() 
 {
-	
 	//draw a background
 	m_background_texture->Render(Vector2D(), SDL_FLIP_NONE);
+
+	my_character->Render();
 }
 
 void GameScreenLevel1::Update(float deltaTime, SDL_Event e)
 {
-
+	//update character
+	my_character->Update(deltaTime, e);
 }
 
 bool GameScreenLevel1::SetUpLevel1()
 {
+	
 	//load texture
 	m_background_texture = new Texture2D(m_renderer);
-	if (!m_background_texture->LoadFromFile("Images/test.bmp"))
+	if (!m_background_texture->LoadFromFile("Images/BackgroundMB.png"))
 	{
 		std::cout << "Failed to load background texture!" << std::endl;
 		return false;
@@ -39,4 +49,8 @@ bool GameScreenLevel1::SetUpLevel1()
 	{
 		return true;
 	}
+
+	//set up player character
+	my_character = new Character(m_renderer, "Images/Mario.png", Vector2D(64, 330));
+	
 }
