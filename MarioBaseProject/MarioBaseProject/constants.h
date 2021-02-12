624d19c5518a3651b52a8ba42c898d650a334047
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-02-12 14:33:44 +0000

    Progress

--- MarioBaseProject/MarioBaseProject/constants.h
+++ MarioBaseProject/MarioBaseProject/constants.h
@@ -2,3 +2,7 @@
 //screen dimensions
 #define SCREEN_WIDTH 512
 #define SCREEN_HEIGHT 416
+#define MOVEMENTSPEED 100.0f
+#define GRAVITY 260.0f
+#define JUMP_FORCE_DECRECEMENT 400.0f
+#define INITIAL_JUMP_FORCE 600.0f
