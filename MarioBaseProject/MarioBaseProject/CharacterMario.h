0dbfca6a936420ca17ff9a6796c45ca60b73ca3c
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-02-19 13:58:52 +0000

    Progress

--- MarioBaseProject/MarioBaseProject/CharacterMario.h
+++ MarioBaseProject/MarioBaseProject/CharacterMario.h
@@ -8,34 +8,22 @@ class CharacterMario : public Character
 {
 protected:
 
-	SDL_Renderer* m_renderer;
-	Vector2D m_position;
-	Texture2D* m_texture;
-	bool m_moving_left;
-	bool m_moving_right;
-	bool m_jumping;
-	bool m_can_jump;
-	float m_jump_force;
 
-	virtual void MoveLeft(float deltaTime);
-	virtual void MoveRight(float deltaTime);
-	virtual void AddGravity(float deltaTime);
-	virtual void Jump();
+
+	void Update(float deltaTime, SDL_Event e) override;
+	
 
 public:
 
 	CharacterMario(SDL_Renderer* renderer, std::string imagePath, Vector2D start_position);
 	~CharacterMario();
 
-	virtual void Render();
-	void Update(float deltaTime, SDL_Event e) override;
-	void SetPosition(Vector2D new_position);
-	Vector2D GetPosition();
+	
 	
 
 private:
 
-	FACING m_facing_direction;
+	
 };
 
 
